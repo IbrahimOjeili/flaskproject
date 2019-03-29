891be92a7104271a5b4d568ecc7ab46030e15718
Ibrahim Ojeili <i.o_1193@hotmail.com>
2019-03-29 15:13:09 +0000

    Add home page

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
-from flask import Flask,request
+from flask import Flask,request,render_template
 app = Flask(__name__)
 
 @app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/api/calc")
 def hello():
     num = float(request.args.get('myVar'))
     print(num)
